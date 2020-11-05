8377cf9812c2796d2d2a53e750db8708801cb18b
chuma1409 <chumagushman14@gmail.com>
2020-11-05 06:21:38 +0200

    table update and factory functins modified

--- table.sql
+++ table.sql
@@ -13,6 +13,19 @@ create table regNumber(
 
 
 
+ create table towns(
+    id serial primary key,
+    town_name text not null,
+    start_string text not null
+);
+
+
+create table regNumbers(
+     id serial primary key,
+   reg_number text not null,
+    town_id int,
+    FOREIGN KEY (town_id) REFERENCES towns(id)
+);
 
 
 create table regNumber(
