61993f61179b5de7751115017fc547ae33f62ca7
chuma1409 <chumagushman14@gmail.com>
2020-11-09 04:36:33 +0200

    fixed test

--- test/registrations.test.js
+++ test/registrations.test.js
@@ -1,6 +1,7 @@
 const assert = require("assert")
 const Registrations =  require("../registrations")
 
+describe("Registrations", function () {
 const pg = require("pg");
 const Pool = pg.Pool;
 const connectionString = process.env.DATABASE_URL || 'postgresql://chuma:pg123@localhost:5432/registrationstest';
@@ -8,7 +9,7 @@ const pool = new Pool({
   connectionString
 });
 
-describe("Registrations", function () {
+
 beforeEach(async function () {
     await pool.query("delete from regNumbers");
 });
